b414d979033b29f3c8a469656f21e61f37ec8c56
Sandra <spadacinisandra@gmail.com>
2024-11-07 14:56:09 +0100

    Home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Service\TrickServiceInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_index')]
-    public function index(): Response
+    public function __construct(TrickServiceInterface $trickService)
     {
-        return $this->render('index.html.twig');
+        $this->trickServiceInterface = $trickService;
+    }
+
+    #[Route(path: '/', name: 'app_home')]
+    public function index(TrickRepository $trickRepository, TrickServiceInterface $trickService): Response
+    {
+        // Récupère les tricks depuis la base de données
+        $tricks = $trickRepository->findAll();
+        $tricksWithImages = [];
+
+        foreach ($tricks as $trick) {
+            $tricksWithImages[] = [
+                'trick' => $trick,
+                'featuredImage' => $this->trickServiceInterface->getFeaturedImageOrDefault($trick),
+            ];
+        }
+
+        // Rendu de la vue Twig en passant les tricks
+        return $this->render('home.html.twig', [
+            'tricks' => $tricksWithImages,
+        ]);
     }
 
 }
