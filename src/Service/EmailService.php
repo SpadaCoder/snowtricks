2dd3eae1c29da3601485e7d3aeaa97bf313cd1fa
Sandra <spadacinisandra@gmail.com>
2024-12-06 10:59:40 +0100

    Reset Password

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -6,17 +6,14 @@ use App\Entity\User;
 use App\Security\EmailVerifier;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class EmailService implements EmailServiceInterface
 {
-    private UrlGeneratorInterface $urlGenerator;
     private EmailVerifier $emailVerifier;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, EmailVerifier $emailVerifier) 
+    public function __construct(EmailVerifier $emailVerifier) 
     {
-        $this->urlGenerator = $urlGenerator;
         $this->emailVerifier = $emailVerifier;
     }
 
@@ -28,6 +25,19 @@ class EmailService implements EmailServiceInterface
             ->to($user->getEmail())
             ->subject('Merci de confirmer votre Email')
             ->htmlTemplate('registration/confirmation_email.html.twig');
+
+        // Utiliser l'EmailVerifier pour gérer l'envoi avec une URL sécurisée
+        $this->emailVerifier->sendEmailConfirmation($routeName, $user, $email);
+    }
+
+    public function sendPasswordResetEmail(User $user, string $routeName, string $fromEmail): void
+    {
+        // Préparer l'email avec un template et les données nécessaires
+        $email = (new TemplatedEmail())
+            ->from(new Address($fromEmail, 'Snowtricks Mail Bot'))
+            ->to($user->getEmail())
+            ->subject('Réinitialisation de mot de passe')
+            ->htmlTemplate('registration/password_reset_email.html.twig');
 
         // Utiliser l'EmailVerifier pour gérer l'envoi avec une URL sécurisée
         $this->emailVerifier->sendEmailConfirmation($routeName, $user, $email);
--- src/Service/UserServiceInterface.php
+++ src/Service/UserServiceInterface.php
@@ -14,4 +14,29 @@ interface UserServiceInterface
      * @param User          $user Instance de l'utilisateur à remplir et enregistrer
      */
     public function processRegistrationForm(FormInterface $form, User $user): void;
+
+    /**
+     * Définit le mot de passe haché d'un utilisateur.
+     *
+     * @param User $user L'utilisateur dont le mot de passe doit être modifié
+     * @param string $plainPassword Le nouveau mot de passe en texte clair
+     */
+    public function setUserPassword(User $user, string $plainPassword): void;
+
+    /**
+     * Marque un utilisateur comme vérifié.
+     *
+     * Cette méthode met à jour le champ correspondant dans l'entité utilisateur
+     * pour indiquer que l'utilisateur a confirmé son email.
+     * 
+     * @param User $user L'utilisateur à vérifier.
+     */
+    public function verifyUser(User $user): void;
+
+    /**
+     * Persiste et enregistre un utilisateur dans la base de données.
+     *
+     * @param User $user L'entité User à persister dans la base de données.
+     */
+    public function save(User $user): void;
 }
